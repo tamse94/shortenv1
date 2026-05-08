31be73fa1a46437ae17b47bd145c7faaada5c7bd
tamse94 <kopetraja254@gmail.com>
2026-05-09 04:14:16 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const dynamic = 'force-dynamic';
 
 export async function generateMetadata() {
   const siteName = await getSetting("site_name") || "ShortenURL";
-  const description = await getSetting("site_description") || "Layanan pemendek URL cepat and aman.";
+  const description = await getSetting("site_description") || "Layanan pemendek URL cepat dan aman.";
   const ogImage = await getSetting("og_image") || "";
 
   return {
@@ -49,7 +49,6 @@ export async function generateMetadata() {
       description: description,
       images: ogImage ? [ogImage] : [],
     },
-    // VERIFIKASI METATAG: Tetap di sini biar kebaca sistem SEO
     other: {
       "monetag": "040fc680c5a16e17f2d1616e679831de"
     }
@@ -58,12 +57,15 @@ export async function generateMetadata() {
 
 export default async function RootLayout({ children }) {
   const siteName = await getSetting("site_name") || "ShortenURL";
-  const adsHead = await getSetting("ads_head") || "";
 
-  // Deteksi Halaman pakai Middleware (Biar gak tembus ke Dashboard/List/Seting)
+  // Deteksi Halaman pakai Middleware (Dibutuhkan untuk blokir iklan di Admin)
   const headerList = headers();
   const pathname = headerList.get('x-pathname') || "";
-  const isAdminPage = pathname.startsWith("/dasbord") || pathname.startsWith("/list") || pathname.startsWith("/seting");
+  
+  // Cek apakah halaman saat ini adalah area admin
+  const isAdminPage = pathname.startsWith("/dasbord") || 
+                      pathname.startsWith("/list") || 
+                      pathname.startsWith("/seting");
 
   return (
     <html lang="en">
@@ -73,29 +75,15 @@ export default async function RootLayout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 
-        {/* SOLUSI MONETAG: Paksa script masuk ke area HEAD secara fisik */}
-        {!isAdminPage && adsHead && (
-          <script
-            id="monetag-head-injector"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function() {
-                  var rawAds = \`${adsHead.replace(/`/g, '\\`').replace(/<\//g, '<\\/')}\`;
-                  var tempDiv = document.createElement('div');
-                  tempDiv.innerHTML = rawAds;
-                  var scripts = tempDiv.getElementsByTagName('script');
-                  for (var i = 0; i < scripts.length; i++) {
-                    var s = document.createElement('script');
-                    for (var j = 0; j < scripts[i].attributes.length; j++) {
-                      s.setAttribute(scripts[i].attributes[j].name, scripts[i].attributes[j].value);
-                    }
-                    if (scripts[i].innerHTML) s.innerHTML = scripts[i].innerHTML;
-                    document.head.appendChild(s);
-                  }
-                })();
-              `
-            }}
-          />
+        {/* 1. PEMASANGAN MANUAL SCRIPT MONETAG DI DALAM HEAD */}
+        {/* Script ini hanya akan muncul jika BUKAN di halaman admin */}
+        {!isAdminPage && (
+          <script 
+            src="https://quge5.com/88/tag.min.js" 
+            data-zone="237527" 
+            async 
+            data-cfasync="false"
+          ></script>
         )}
       </head>
       <body style={{ backgroundColor: '#f5f5f5', display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
@@ -110,7 +98,7 @@ export default async function RootLayout({ children }) {
 
         <Footer siteName={siteName} />
 
-        {/* Script Bootstrap & jQuery tetap di body biar web gak lemot */}
+        {/* Script pendukung Bootstrap */}
         <Script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" strategy="lazyOnload" />
         <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" strategy="lazyOnload" />
       </body>
