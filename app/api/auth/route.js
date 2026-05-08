6fd945ba04ae9b3131b7ab73591983c05c926ba6
tamse94 <kopetraja254@gmail.com>
2026-05-09 04:41:12 +0700

    Update route.js

--- app/api/auth/route.js
+++ app/api/auth/route.js
@@ -4,24 +4,23 @@ import { cookies } from "next/headers";
 export async function POST(request) {
   const { email, password } = await request.json();
 
-  // GANTI INI SAMA EMAIL DAN PASSWORD LOGIN LO NANTI
-  const ADMIN_EMAIL = "admin@domain.com";
-  const ADMIN_PASS = "admin123";
+  // Sekarang kita ambil dari Environment Variables Vercel
+  const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+  const ADMIN_PASS = process.env.ADMIN_PASSWORD;
 
   if (email === ADMIN_EMAIL && password === ADMIN_PASS) {
-    // Set cookie 'admin_token' sebagai tanda udah login (Aktif 1 hari)
+    // Set cookie 'admin_token'
     cookies().set("admin_token", "super-secret-token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60 * 24, // 24 jam
+      maxAge: 60 * 60 * 24, 
       path: "/",
     });
     return NextResponse.json({ success: true });
   }
 
-  // Kalau salah, tolak!
   return NextResponse.json(
-    { success: false, message: "Email atau password salah boss!" }, 
+    { success: false, message: "Invalid email or password!" }, 
     { status: 401 }
   );
 }
