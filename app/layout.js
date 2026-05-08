a69aaadd4e466a3756b168d54c66180a49beb248
tamse94 <kopetraja254@gmail.com>
2026-05-09 04:08:51 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const dynamic = 'force-dynamic';
 
 export async function generateMetadata() {
   const siteName = await getSetting("site_name") || "ShortenURL";
-  const description = await getSetting("site_description") || "Layanan pemendek URL cepat dan aman.";
+  const description = await getSetting("site_description") || "Layanan pemendek URL cepat and aman.";
   const ogImage = await getSetting("og_image") || "";
 
   return {
@@ -19,9 +19,7 @@ export async function generateMetadata() {
       template: `%s | ${siteName}`,
     },
     description: description,
-    alternates: {
-      canonical: '/', 
-    },
+    alternates: { canonical: '/' },
     robots: {
       index: true,
       follow: true,
@@ -51,7 +49,7 @@ export async function generateMetadata() {
       description: description,
       images: ogImage ? [ogImage] : [],
     },
-    // 1. SOLUSI VERIFIKASI MONETAG: Wajib di sini biar jadi Meta Tag Permanen
+    // VERIFIKASI METATAG: Tetap di sini biar kebaca sistem SEO
     other: {
       "monetag": "040fc680c5a16e17f2d1616e679831de"
     }
@@ -62,7 +60,7 @@ export default async function RootLayout({ children }) {
   const siteName = await getSetting("site_name") || "ShortenURL";
   const adsHead = await getSetting("ads_head") || "";
 
-  // Deteksi Halaman pakai Middleware
+  // Deteksi Halaman pakai Middleware (Biar gak tembus ke Dashboard/List/Seting)
   const headerList = headers();
   const pathname = headerList.get('x-pathname') || "";
   const isAdminPage = pathname.startsWith("/dasbord") || pathname.startsWith("/list") || pathname.startsWith("/seting");
@@ -74,6 +72,31 @@ export default async function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+        {/* SOLUSI MONETAG: Paksa script masuk ke area HEAD secara fisik */}
+        {!isAdminPage && adsHead && (
+          <script
+            id="monetag-head-injector"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  var rawAds = \`${adsHead.replace(/`/g, '\\`').replace(/<\//g, '<\\/')}\`;
+                  var tempDiv = document.createElement('div');
+                  tempDiv.innerHTML = rawAds;
+                  var scripts = tempDiv.getElementsByTagName('script');
+                  for (var i = 0; i < scripts.length; i++) {
+                    var s = document.createElement('script');
+                    for (var j = 0; j < scripts[i].attributes.length; j++) {
+                      s.setAttribute(scripts[i].attributes[j].name, scripts[i].attributes[j].value);
+                    }
+                    if (scripts[i].innerHTML) s.innerHTML = scripts[i].innerHTML;
+                    document.head.appendChild(s);
+                  }
+                })();
+              `
+            }}
+          />
+        )}
       </head>
       <body style={{ backgroundColor: '#f5f5f5', display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         
@@ -87,29 +110,9 @@ export default async function RootLayout({ children }) {
 
         <Footer siteName={siteName} />
 
+        {/* Script Bootstrap & jQuery tetap di body biar web gak lemot */}
         <Script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" strategy="lazyOnload" />
         <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" strategy="lazyOnload" />
-
-        {/* 2. SOLUSI IKLAN GAK MUNCUL: Trik Injector Javascript */}
-        {/* Trik ini memaksa tag <script> dari database untuk tereksekusi dan ditaruh di <head> */}
-        {!isAdminPage && adsHead && (
-          <Script id="ads-injector" strategy="afterInteractive" dangerouslySetInnerHTML={{
-            __html: `
-              var temp = document.createElement('div');
-              temp.innerHTML = \`${adsHead.replace(/`/g, '\\`').replace(/<\//g, '<\\/')}\`;
-              var scripts = temp.getElementsByTagName('script');
-              for (var i = 0; i < scripts.length; i++) {
-                var s = document.createElement('script');
-                for (var j = 0; j < scripts[i].attributes.length; j++) {
-                  s.setAttribute(scripts[i].attributes[j].name, scripts[i].attributes[j].value);
-                }
-                s.innerHTML = scripts[i].innerHTML;
-                document.head.appendChild(s);
-              }
-            `
-          }} />
-        )}
-
       </body>
     </html>
   );
